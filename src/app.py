bbdc2a2191f0d4fdc6c8e3f98a22b22131fc6ae1
Alfredo Sanz <alfredo.sanz@bridg-ou.com>
2025-01-07 17:47:37 +0100

    Big commit. Corrected text decodification. Included more context to the retrieved nodes. General improvements.

--- src/app.py
+++ src/app.py
@@ -1,85 +1,22 @@
 import streamlit as st
 
-# from dotenv import load_dotenv
-from llama_index.llms.openai import OpenAI
-# from langchain_openai import ChatOpenAI
-
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
-# from chatbot import build_chatbot
-from chatbot import load_chroma_index
-
-from pytubefix import YouTube
-
 from pathlib import Path
 import os
+
+from chatbot import load_index, get_chat_engine
 
 
 DATA_DIR = Path("data")
 
 DEFAULT_THREAD = {"configurable": {"thread_id": "default"}} # For the chatbot. Each thread is a different conversation
 
-CHROMA_DB_PATH = DATA_DIR / "chroma"
-CHROMA_COLLECTION_NAME = "libre-a-los-30-videos"
-
-CHAT_MODES = ["best", "condense_question", "context", "condense_plus_context"]
 MODELS = ["gpt-4o", "gpt-4o-mini", "o1-mini", "o1-preview"]
-RESPONSE_MODES = ["compact", "refine", "tree_summarize", "accumulate", "compact_accumulate"]
-
-# TRANSCRIPT_PATHS = [
-#     DATA_DIR / "mZvnATAawpM.txt",
-#     DATA_DIR / "5NUGCNhfZ4I.txt",
-# ]
-
-# VIDEO_TITLES = [
-#     "Cómo Comprar Pisos sin poner Dinero - Un Inmueble por cada Día del Mes - Alex Emebe Ep:16",
-#     "Libre a los 30 - Javier Medina I Podcast #50"
-# ]
-
-# DEFAULT_MODEL = "gpt-4o-mini"
-# DEFAULT_PROMPT = "You are a useful agent that has access to the community knowledge"
 
 
 # OpenAI API key
+from llama_index.llms.openai import OpenAI
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-llm = OpenAI(temperature=0.1, model="gpt-4")
 
-
-# [ST] App basics
-st.set_page_config(page_title="LibreBot", page_icon="🤖")
-st.title("Accede a nuestro conocimiento acumulado")
-
-
-# Initialize chatbot funtion
-# def initialize_chatbot(system_prompt, model):
-    
-#     llm = ChatOpenAI(model=model)
-
-#     st.session_state['chatbot'] = build_chatbot(llm)
-
-#     transcript = ""
-#     for path, title in zip(TRANSCRIPT_PATHS, VIDEO_TITLES):
-#         if path.exists():
-#             with open(path, "r", encoding = "ISO-8859-1") as f:
-#                 transcript += f"Title: {title}\n"
-#                 transcript += f.read()
-#                 transcript += "\n\n"
-
-#     full_prompt = f"""{system_prompt}\n
-#     {transcript}"""
-
-#     st.session_state['chatbot'].update_state(DEFAULT_THREAD, {"messages": [("system", full_prompt)]})
-
-# Chatbot response stream helper function
-# def chatbot_response(user_query: str):
-#     for msg, metadata in st.session_state['chatbot'].stream(
-#         {"messages": [("user", user_query)]},
-#         config = DEFAULT_THREAD,
-#         stream_mode="messages" # For some reason you have to use the NOT DOCUMENTED stream_mode="messages" argument
-#         ): 
-#         yield msg.content
 
 def render_chat(debug_mode=False):
     for message in st.session_state["chat_history"]:
@@ -97,31 +34,10 @@ def render_chat(debug_mode=False):
                 with st.expander("Retrieved nodes"):
                     st.write(message["content"])
 
-        # elif message.role == "assistant":
 
-        #     if message.content is not None and message.content != "":
-            
-        #         if "tool_calls" in message.additional_kwargs:
-        #             tool_calls_dicts = message.additional_kwargs["tool_calls"]
-
-        #             with st.chat_message("tool"):
-        #                 with st.expander("Retrieved nodes"):
-        #                     for t, tool_call in enumerate(tool_calls_dicts):
-        #                         st.write(f"Tool call {t+1}: {tool_call['input']}")
-        #                         st.write("\n\n".join([str(n) for n in tool_call["retrieved_nodes"]]))
-
-        #         with st.chat_message("ai"):
-        #             st.write(message.content)
-
-        # elif message.role == "tool" and debug_mode:
-
-        #     node_dicts = message.additional_kwargs.get("retrieved_nodes")
-
-        #     if node_dicts is not None:
-        #         with st.chat_message("tool"):
-        #             with st.expander("Retrieved nodes"):
-        #                 st.write("\n\n".join([str(n) for n in node_dicts]))
-
+# [ST] App basics
+st.set_page_config(page_title="LibreBot", page_icon="🤖")
+st.title("Accede a nuestro conocimiento acumulado")
 
 # [ST] Initialize config in session state
 if "debug_mode" not in st.session_state:
@@ -133,16 +49,12 @@ if "chat_history" not in st.session_state:
 
 # [ST] Initialize index in session state
 if "index" not in st.session_state:
-    st.session_state["index"] = load_chroma_index(data_dir=str(CHROMA_DB_PATH), collection_name=CHROMA_COLLECTION_NAME)
+    st.session_state["index"] = load_index(data_dir=str(DATA_DIR))
 
 
 # [ST] Sidebar
 with st.sidebar:
     st.title("Settings")
-
-    # st.subheader("Prompt settings")
-    # st.text_area("System prompt", value=DEFAULT_PROMPT, key="system_prompt", height=200)
-    # st.write("Note that the video transcripts are supplied after this prompt.")
 
     st.subheader("Chatbot settings")
 
@@ -150,13 +62,12 @@ with st.sidebar:
     temperature = st.slider("Temperature", min_value=0.0, max_value=1.0, value=1.0, step=0.01)
     llm = OpenAI(temperature=temperature, model=model)
 
-    response_mode = st.selectbox("Response mode", RESPONSE_MODES, index=0)
-    top_k = st.slider("Top k", min_value=1, max_value=30, value=4, step=1)
-
-    chat_mode = st.selectbox("Chat mode", CHAT_MODES, index=0)
+    top_k = st.slider("Retrieve top K", min_value=1, max_value=30, value=4, step=1)
+    back_context = st.slider("Backward context", min_value=0, max_value=100, value=10, step=1)
+    forw_context = st.slider("Forward context", min_value=0, max_value=100, value=20, step=1)
 
     if st.button("Apply + Restart"):
-        st.session_state["chatbot"] = st.session_state["index"].as_chat_engine(chat_mode=chat_mode, llm=llm, response_mode=response_mode, similarity_top_k=top_k)
+        st.session_state["chatbot"] = get_chat_engine(st.session_state["index"], llm, top_k, back_context, forw_context)
 
     st.subheader("Verbosity settings")
 
@@ -165,56 +76,53 @@ with st.sidebar:
 
 # [ST] Initialize chatbot in session state
 if "chatbot" not in st.session_state:
-    st.session_state["chatbot"] = st.session_state["index"].as_chat_engine(chat_mode=chat_mode, llm=llm, response_mode=response_mode, similarity_top_k=top_k)
+    st.session_state["chatbot"] = get_chat_engine(st.session_state["index"], llm, top_k, back_context, forw_context)
 
 
 # [ST] Write previous chat messages
 render_chat(debug_mode=st.session_state["debug_mode"])          
-
 
 # [ST] Get user input, answer, and write to chat
 user_query = st.chat_input("¿En qué puedo ayudarte?")
 
 if user_query is not None and user_query != "":
 
-    messages = [{"role": "user", "content": user_query}]
+    # User
+    new_messages = []
+    new_messages.append({"role": "user", "content": user_query})
 
     with st.chat_message("user"):
         st.markdown(user_query)
 
     response = st.session_state["chatbot"].stream_chat(user_query)
 
-    if len(response.source_nodes) > 0:
+    # Tool
+    for tool_call in response.sources:
+        
+        node_dicts = [{"score": source_node.score, 
+                       "text": source_node.text, 
+                       "metadata": source_node.metadata} 
 
-        tool_call_msg = None
-        for message in st.session_state["chatbot"].chat_history[::-1]:
-            if message.role == "assistant" and "tool_calls" in message.additional_kwargs:
-                tool_call_msg = message
-                break
+                       for source_node in tool_call.raw_output.source_nodes]
+        
+        tool_call_str = ""
+        tool_call_str += f"Tool call: {tool_call.raw_input}\n\n"
+        tool_call_str += "\n\n".join([str(n) for n in node_dicts])
+        tool_call_str += "\n\n\n\n\n"
 
-        tool_calls_dicts = []
-        for t, tool_call in enumerate(tool_call_msg.additional_kwargs["tool_calls"]):
-            node_dicts = [{"score": n.score, "text": n.text, "metadata": n.metadata} for n in response.source_nodes[t*top_k:(t+1)*top_k]]
-            tool_input = tool_call.function.arguments
-            tool_calls_dicts.append({"input": tool_input, "retrieved_nodes": node_dicts})
+        new_messages.append({"role": "tool", "content": tool_call_str})
 
-        tool_calls_str = ""
-        for t, tool_call in enumerate(tool_calls_dicts):
-            tool_calls_str += f"Tool call {t+1}: {tool_call['input']}\n\n"
-            tool_calls_str += "\n\n".join([str(n) for n in tool_call["retrieved_nodes"]])
-            tool_calls_str += "\n\n\n\n\n"
-        tool_calls_str = tool_calls_str.strip()
-
-        messages.append({"role": "tool", "content": tool_calls_str})
-
-        if st.session_state["debug_mode"]:
-            with st.chat_message("tool"):
-                with st.expander("Retrieved nodes"):
-                    st.write(tool_calls_str)
-
+    if st.session_state["debug_mode"]:
+        for message in new_messages:
+            if message["role"] == "tool":
+                with st.chat_message("tool"):
+                    with st.expander("Retrieved nodes"):
+                        st.write(message["content"])
+        
+    # QA LLM
     with st.chat_message("ai"):
         st.write_stream(response.response_gen)
     
-    messages.append({"role": "assistant", "content": response.response})
+    new_messages.append({"role": "assistant", "content": response.response})
 
-    st.session_state["chat_history"].extend(messages)
+    st.session_state["chat_history"].extend(new_messages)
--- src/chatbot.py
+++ src/chatbot.py
@@ -1,52 +1,210 @@
-from pathlib import Path
-
-import chromadb
-from llama_index.vector_stores.chroma import ChromaVectorStore
-from llama_index.core import StorageContext
-from llama_index.core import VectorStoreIndex
-
-from llama_index.core import get_response_synthesizer
+from typing import List, Optional
+from llama_index.core import QueryBundle, StorageContext, load_indices_from_storage, get_response_synthesizer
+from llama_index.core.postprocessor.types import BaseNodePostprocessor
+from llama_index.core.schema import NodeRelationship, NodeWithScore, TextNode
+from llama_index.core.storage.docstore import BaseDocumentStore
+from llama_index.core.bridge.pydantic import Field
+from llama_index.core.agent import AgentRunner
+from llama_index.core.tools.query_engine import QueryEngineTool
 from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.core.query_engine import RetrieverQueryEngine
 
-from typing import Annotated
-from pydantic import BaseModel
 
-from langgraph.graph.message import add_messages
-from langgraph.graph import StateGraph
-from langgraph.checkpoint.memory import MemorySaver
+# from typing import Annotated
+# from pydantic import BaseModel
 
-def build_chatbot(llm):
+# from langgraph.graph.message import add_messages
+# from langgraph.graph import StateGraph
+# from langgraph.checkpoint.memory import MemorySaver
+
+
+# def build_chatbot(llm):
     
-    class State(BaseModel):
-        messages: Annotated[list, add_messages]
+#     class State(BaseModel):
+#         messages: Annotated[list, add_messages]
 
-    graph_builder = StateGraph(State)
+#     graph_builder = StateGraph(State)
 
-    def chatbot_node(state: State):
-        return {"messages": [llm.invoke(state.messages)]}
+#     def chatbot_node(state: State):
+#         return {"messages": [llm.invoke(state.messages)]}
 
-    graph_builder.add_node("chatbot", chatbot_node)
+#     graph_builder.add_node("chatbot", chatbot_node)
 
-    graph_builder.set_entry_point("chatbot")
-    graph_builder.set_finish_point("chatbot")
+#     graph_builder.set_entry_point("chatbot")
+#     graph_builder.set_finish_point("chatbot")
 
-    memory = MemorySaver()
-    return graph_builder.compile(checkpointer=memory)
+#     memory = MemorySaver()
+#     return graph_builder.compile(checkpointer=memory)
 
-def load_chroma_index(data_dir: str, collection_name: str):
+# def chatbot_response(user_query: str):
+#     for msg, metadata in st.session_state['chatbot'].stream(
+#         {"messages": [("user", user_query)]},
+#         config = DEFAULT_THREAD,
+#         stream_mode="messages" # For some reason you have to use the NOT DOCUMENTED stream_mode="messages" argument
+#         ): 
+#         yield msg.content
 
-    db = chromadb.PersistentClient(path=data_dir)
-    chroma_collection = db.get_or_create_collection(collection_name)
 
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+def load_index(data_dir: str):
+    storage_context = StorageContext.from_defaults(persist_dir="./data")
+    indices = load_indices_from_storage(storage_context)
+    return indices[0]
 
-    return VectorStoreIndex.from_vector_store(
-        vector_store, storage_context=storage_context
-    )
 
-def get_chat_engine(index, top_k: int, llm, response_mode: str, chat_mode: str):
+class Timestamp:
+    def __init__(self, time_str: str):
+        self.hours, self.minutes, self.seconds = map(int, time_str.split(':'))
+
+    def __lt__(self, other):
+        return (self.hours, self.minutes, self.seconds) < (other.hours, other.minutes, other.seconds)
+
+    def __le__(self, other):
+        return (self.hours, self.minutes, self.seconds) <= (other.hours, other.minutes, other.seconds)
+
+    def __eq__(self, other):
+        return (self.hours, self.minutes, self.seconds) == (other.hours, other.minutes, other.seconds)
+
+    def __ne__(self, other):
+        return (self.hours, self.minutes, self.seconds) != (other.hours, other.minutes, other.seconds)
+
+    def __gt__(self, other):
+        return (self.hours, self.minutes, self.seconds) > (other.hours, other.minutes, other.seconds)
+
+    def __ge__(self, other):
+        return (self.hours, self.minutes, self.seconds) >= (other.hours, other.minutes, other.seconds)
+    
+    def to_seconds(self):
+        return self.hours * 3600 + self.minutes * 60 + self.seconds
+
+
+class ContextInflator(BaseNodePostprocessor):
+    
+    docstore: BaseDocumentStore
+    num_backward_nodes: int = Field(default=1)
+    num_forward_nodes: int = Field(default=1)
+    timestamp_keys: list[str] = Field(default=['start_timestamp', 'end_timestamp'])
+    proximity_merge: int = Field(default=0) # seconds
+
+    def get_previous_node(self, node):
+        prev_node_ref = node.relationships.get(NodeRelationship.PREVIOUS)
+        
+        if prev_node_ref is None:
+            return None
+        
+        return self.docstore.get_node(prev_node_ref.node_id)
+
+    def get_next_node(self, node):
+        next_node_ref = node.relationships.get(NodeRelationship.NEXT)
+        
+        if next_node_ref is None:
+            return None
+        
+        return self.docstore.get_node(next_node_ref.node_id)
+
+    def _postprocess_nodes(
+            self, 
+            nodes: List[NodeWithScore], 
+            query_bundle: Optional[QueryBundle]
+    ) -> List[NodeWithScore]:
+        
+        # Find all unique parent nodes
+        parent_nodes = [] # Non-repeating list of parent nodes' ids
+        for node in nodes:
+            parent_node = node.node.relationships.get(NodeRelationship.PARENT)
+            if parent_node is not None and parent_node.node_id not in parent_nodes:
+                parent_nodes.append(parent_node.node_id)
+
+        # Create a nodes containing the context for the children nodes
+        context_nodes = []
+        for parent_node in parent_nodes:
+            child_nodes = [node for node in nodes if node.node.relationships.get(NodeRelationship.PARENT).node_id == parent_node]
+            child_nodes.sort(key=lambda x: Timestamp(x.node.metadata[self.timestamp_keys[0]]))
+            
+            # Jump a series of nodes from each child node to find its context
+            context_snippets = [] # List of tuples of 2 nodes defining a context snippet, ordered in time
+            for node in child_nodes:
+
+                # Get node num_backward_nodes steps before
+                start_node = node.node
+                for i in range(self.num_backward_nodes):
+                    prev_node = self.get_previous_node(start_node)
+                    if prev_node is None:
+                        break
+                    start_node = prev_node
+
+                # Get node num_forward_nodes steps after
+                end_node = node.node
+                for i in range(self.num_forward_nodes):
+                    next_node = self.get_next_node(end_node)
+                    if next_node is None:
+                        break
+                    end_node = next_node
+
+                context_snippets.append((start_node, end_node))
+            
+            # Find where is it worth merging the context of two child nodes
+            merged_snippets = [] # List of merged snippets due to overlap or proximity
+            previous_snippet = None
+            for snippet in context_snippets:
+                if previous_snippet is None:
+                    previous_snippet = snippet
+                    continue
+                
+                # Overlapping merge
+                if Timestamp(snippet[0].metadata[self.timestamp_keys[0]]) <= Timestamp(previous_snippet[1].metadata[self.timestamp_keys[1]]):
+                    previous_snippet = (previous_snippet[0], snippet[1])
+
+                # Proximity merge
+                elif Timestamp(snippet[0].metadata[self.timestamp_keys[0]]).to_seconds() - Timestamp(previous_snippet[1].metadata[self.timestamp_keys[1]]).to_seconds() <= self.proximity_merge:
+                    previous_snippet = (previous_snippet[0], snippet[1])
+
+                else:
+                    merged_snippets.append(previous_snippet)
+                    previous_snippet = snippet
+
+            merged_snippets.append(previous_snippet)
+
+            # Extract actual text for each snippet as well as the timestamps
+            snippet_texts = []
+            snippet_timestamps = []
+            source_text = self.docstore.get_node(parent_node).text
+            for snippet in merged_snippets:
+                start_text = snippet[0].text.strip()
+                end_text = snippet[1].text.strip()
+
+                start_index = source_text.find(start_text)
+                end_index = source_text.find(end_text) + len(end_text)
+
+                if start_index == -1 or end_index == -1:
+                    print('Warning: Text not found in parent node.')
+
+                cropped_text = source_text[start_index:end_index]
+
+                snippet_texts.append(cropped_text)
+                snippet_timestamps.append((snippet[0].metadata[self.timestamp_keys[0]], snippet[1].metadata[self.timestamp_keys[1]]))
+
+            # Create composed text
+            _ = [(f'[{timestamp[0]}]\n' + text + f'\n[{timestamp[1]}]\n') for text, timestamp in zip(snippet_texts, snippet_timestamps)]
+            composed_text = '(...)\n'.join(_)
+
+            # Create context node
+            text_node = TextNode(
+                text=composed_text,
+                text_template='{content}\n\nRelevant quotes\' timestamps:\n',
+            )
+
+            context_node = NodeWithScore(node=text_node)
+            context_nodes.append(context_node)
+
+        return context_nodes + nodes
+
+def get_chat_engine(
+        index, llm, 
+        top_k: int = 2, 
+        back_context: int = 10, 
+        forw_context: int = 20, 
+        response_mode: str = 'compact',  
+        proximity_merge: int = 15):
 
     retriever = VectorIndexRetriever(
         index=index,
@@ -61,7 +219,21 @@ def get_chat_engine(index, top_k: int, llm, response_mode: str, chat_mode: str):
     query_engine = RetrieverQueryEngine(
         retriever=retriever,
         response_synthesizer=response_synthesizer,
-        node_postprocessors=[],
+        node_postprocessors=[
+            ContextInflator(
+                docstore=index.docstore,
+                num_backward_nodes=back_context,
+                num_forward_nodes=forw_context,
+                proximity_merge=proximity_merge
+            )
+        ],
     )
 
-    return index.as_chat_engine(query_engine=query_engine, chat_mode=chat_mode)
+    query_engine_tool = QueryEngineTool.from_defaults(query_engine=query_engine)
+
+    chat_engine = AgentRunner.from_llm(
+        tools=[query_engine_tool],
+        llm=llm
+    )
+
+    return chat_engine
